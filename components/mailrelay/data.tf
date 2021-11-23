4ee0ad75ebe2d196d6158cc98e325543c7debf7e
taherkhan30 <taher.khan@hmcts.net>
2021-11-23 12:11:04 +0000

    added configurations for mail relay managed identity

--- components/mailrelay/data.tf
+++ components/mailrelay/data.tf
@@ -3,3 +3,13 @@ data "azurerm_subscription" "current" {
 
 data "azurerm_client_config" "current" {
 }
+
+data "azurerm_key_vault" "acme" {
+  name                = "acmedtssdsprod"
+  resource_group_name = "sds-platform-prod-rg"
+}
+
+data "azurerm_user_assigned_identity" "mailrelay_mi" {
+  name                = "mailrelay-${var.env}-mi"
+  resource_group_name = "managed-identities-dev-rg"
+}
