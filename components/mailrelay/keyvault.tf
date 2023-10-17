38a77a188455c0dae751a7c52bcbe157bb17fb40
Vik <viktoriya.bogdanova@hmcts.net>
2023-10-17 15:12:36 +0100

    add MI for stg

--- components/mailrelay/keyvault.tf
+++ components/mailrelay/keyvault.tf
@@ -52,7 +52,7 @@ resource "azurerm_user_assigned_identity" "managed_identity" {
 resource "azurerm_key_vault_access_policy" "managed_identity_access_policy" {
   key_vault_id = module.azurekeyvault.key_vault_id
 
-  object_id = azurerm_user_assigned_identity.managed_identity[count.index].principal_id
+  object_id = azurerm_user_assigned_identity.managed_identity.principal_id
   tenant_id = data.azurerm_client_config.current.tenant_id
 
   key_permissions = [
