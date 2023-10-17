7fca444c1e9058301396b4472938245face62c8c
Vik <viktoriya.bogdanova@hmcts.net>
2023-10-17 10:26:24 +0100

    add MI for stg

--- components/mailrelay/keyvault.tf
+++ components/mailrelay/keyvault.tf
@@ -19,12 +19,49 @@ module "azurekeyvault" {
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = var.product_group_object_id
   common_tags             = module.ctags.common_tags
-  # create_managed_identity = true
-  object_id = data.azurerm_client_config.current.object_id
+  create_managed_identity = true
+  object_id               = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "acme" {
   scope                = data.azurerm_key_vault.acme.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azurerm_user_assigned_identity.mailrelay_mi.principal_id
+}
+
+provider "azurerm" {
+  subscription_id            = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
+  skip_provider_registration = "true"
+  features {}
+  alias = "managed_identity_infra_sub"
+}
+
+resource "azurerm_user_assigned_identity" "managed_identity" {
+  provider            = azurerm.managed_identity_infra_sub
+  name                = "${var.product}-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+  location            = var.location
+  tags                = module.ctags.common_tags
+}
+
+resource "azurerm_key_vault_access_policy" "managed_identity_access_policy" {
+  key_vault_id = module.azurekeyvault.key_vault_id
+
+  object_id = azurerm_user_assigned_identity.managed_identity.principal_id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  key_permissions = [
+    "Get",
+    "List",
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
 }
